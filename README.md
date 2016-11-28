ff436611f196d49b810cfa61f8d3370726b4bcbe
datagit <datagit@yahoo.com>
2016-11-29 06:26:08 +0700

    add-platform android

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # simple-todos-meteor-react
+# setup: 
+    $meteor add
+    $meteor npm install
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,6 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 
+import Tasks from '../api/tasks.jsx';
+
 export default class Task extends Component {
+    toggleChecked() {
+
+    }
+
     render() {
         return (
             <li>{this.props.task.text}</li>
